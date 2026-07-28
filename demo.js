f434bdb901b4d99c9b8bfea364175bbf35cdd795
Anekvarna <anek.varna030@gmail.com>
2026-07-28 15:39:23 +0530

    Objects in js

---
+++ demo.js
@@ -0,0 +1,9 @@
+const student={
+    name:"Anekvarna",
+    age:20,
+    branch:"CSE"
+}
+const {name:newname,age,branch}=student;
+console.log("Name=",newname);
+console.log("Age=",age);
+console.log("Branch=",branch);
