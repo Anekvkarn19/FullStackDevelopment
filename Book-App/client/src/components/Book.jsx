ae56a73a100f25188b9f83496ef78bced1cbeaa0
Anekvarna <anek.varna030@gmail.com>
2026-09-01 15:47:12 +0530

    App using react vite

--- Book-App/client/src/components/Book.jsx
+++ Book-App/client/src/components/Book.jsx
@@ -4,7 +4,7 @@ const Book = ({props}) => {
       <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwOTppcCu_AJmVC5fYKckbX0TlCbNPKszxColRd7_Jjw&s" width={100} height={100} alt="Book Image" />
       <h2>Title: {props.title}</h2>
       <h2>Price: ₹{props.price}/-</h2>
-      <button>Add To Cart</button>
+      <button>Add to Cart</button>
     </div>
   )
 }
