eb56c56da5b707a080348668a29b7a7b90a915bd
Anekvarna <anek.varna030@gmail.com>
2026-09-08 15:43:51 +0530

    Book app using npm

---
+++ Book-npm-app/App.jsx
@@ -0,0 +1,14 @@
+import Book from "./Book";
+function App(){
+        return(
+            <div className="bookstore">
+                <h1>
+                    <center>My Book Store</center>
+                </h1>
+                <div>
+                         <Book />
+                </div>
+            </div>
+        )
+    }
+    export default App;
